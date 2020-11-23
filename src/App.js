62680a99e485b01da85564695fbe0a8da4cd801f
faycel-gattoufi <gattoufi.faycel@live.com>
2020-11-23 15:17:49 +0100

    First commit Message

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar,FormControl,NavDropdown,Nav,Button,Form, Row,Container,Col } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+    <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+
+
+<Form className="App-header">
+<Container>
+  <Row>
+  <Col>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="name@example.com" />
+  </Form.Group>
+  </Col>
+    <Col>
+  
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Example select</Form.Label>
+    <Form.Control as="select">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+  </Form.Group>
+  </Col>
+  </Row>
+  <Row>
+    <Col>
+  <Form.Group controlId="exampleForm.ControlSelect2">
+    <Form.Label>Example multiple select</Form.Label>
+    <Form.Control as="select" multiple>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+  </Form.Group>
+  </Col>
+ 
+    <Col>
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Example textarea</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+  </Col>
+  </Row>
+  <Row>
+  <Col>
+  <Form.Group>
+    <Form.File id="exampleFormControlFile1" label="Example file input" />
+  </Form.Group>
+  </Col>
+    <Col>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+  </Col>
+  </Row>
+  </Container>
+</Form>
+</>
+      
+  );
+}
+
+export default App;
